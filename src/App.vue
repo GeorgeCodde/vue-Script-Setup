68e5eb684208f32647d612acc244040bf437b339
JORGE LOPEZ SANCHEZ <jorgels120878@gmail.com>
2023-04-18 19:40:12 -0600

    Version basica de script setup

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { ref } from "vue";
+const counter = ref(0);
+
+// export default defineComponent({
+//   setup() {
+//     const counter = ref(0);
+//     const increase = () => {
+//       counter.value++;
+//     };
+//     return {
+//       counter,
+//       increase,
+//     };
+//   },
+// });
+</script>
+
+<template>
+  <div class="wrapper">
+    <h1>Counter: {{ counter }}</h1>
+    <button @click="counter++">+1</button>
+  </div>
+</template>
+
+<style scoped>
+.wrapper {
+  display: flex;
+  flex-direction: column;
+}
+</style>
